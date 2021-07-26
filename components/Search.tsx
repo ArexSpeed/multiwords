5744024847b9fc62d8b94d9933edee23dbc74383
Arek Cichocki <arexspeed95@gmail.com>
2021-07-26 19:05:41 +0200

    search words ui

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import InvertColorsIcon from '@material-ui/icons/InvertColors';
 import { useAppSelector, useAppDispatch } from 'redux/hooks';
 import { toogleTheme, selectTheme } from 'redux/slices/settingsSlice';
+import { words } from 'data';
+
+//Types
+type Word = {
+  eng: string;
+  pol: string;
+  ger: string;
+  ned: string;
+  spa: string;
+  fra: string;
+  ita: string;
+};
 
 const Search = () => {
   const theme = useAppSelector(selectTheme);
   const dispatch = useAppDispatch();
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [foundWords, setFoundWords] = useState<Word[]>([]);
+
+  useEffect(() => {
+    let tempWords: Array<Word> = [];
+    if (searchValue.length > 2) {
+      words
+        .filter((word) => word.pol.toLowerCase().includes(searchValue.toLowerCase()))
+        .map((item) =>
+          tempWords.push({
+            eng: item.eng,
+            pol: item.pol,
+            ger: item.ger,
+            ned: item.ned,
+            spa: item.spa,
+            fra: item.fra,
+            ita: item.ita
+          })
+        );
+      setFoundWords(tempWords);
+    } else {
+      setFoundWords([]);
+    }
+    console.log(searchValue, 'searchValue');
+    console.log(foundWords, 'word find');
+  }, [searchValue]);
 
   const handleToogle = () => {
     switch (theme) {
@@ -34,6 +72,8 @@ const Search = () => {
             type="text"
             className="w-full bg-transparent text-gray-700 outline-none placeholder:text-primary dark:text-gray-300"
             placeholder="Search a word"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
           />
         </div>
         <div className="flex justify-center items-center">
@@ -42,6 +82,40 @@ const Search = () => {
           </button>
         </div>
       </header>
+      {searchValue.length > 2 && (
+        <div className="flex flex-col justify-start items-start w-full p-2 bg-secondaryLight rounded-sm overflow-scroll dark:bg-secondaryDark">
+          <table>
+            <thead>
+              <tr>
+                <td>ENG</td>
+                <td>POL</td>
+                <td>GER</td>
+                <td>NED</td>
+                <td>SPA</td>
+                <td>FRA</td>
+                <td>ITA</td>
+              </tr>
+            </thead>
+            <tbody>
+              {foundWords.length > 0 ? (
+                foundWords.map((word, i) => (
+                  <tr key={i}>
+                    <td className="text-eng px-2">{word.eng}</td>
+                    <td className="text-pol px-2">{word.pol}</td>
+                    <td className="text-ger px-2">{word.ger}</td>
+                    <td className="text-ned px-2">{word.ned}</td>
+                    <td className="text-spa px-2">{word.spa}</td>
+                    <td className="text-fra px-2">{word.fra}</td>
+                    <td className="text-ita px-2">{word.ita}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>No results</tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      )}
     </section>
   );
 };
