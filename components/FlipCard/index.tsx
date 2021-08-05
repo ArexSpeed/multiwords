78b097c97a44243cf7625fb967e1608ee2a78f82
Arek Cichocki <arexspeed95@gmail.com>
2021-08-05 20:00:00 +0200

    check flipped and points

--- components/FlipCard/index.tsx
+++ components/FlipCard/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
-const FlipCard = ({ onClick, card, index, isInactive, isFlipped, isDisabled }) => {
+type Word = {
+  [key: string]: string;
+};
+interface Props {
+  onClick: (index) => void;
+  word: Word;
+  index: number;
+  isInactive: boolean;
+  isFlipped: boolean;
+}
+
+const FlipCard: React.FC<Props> = ({ onClick, word, index, isInactive, isFlipped }) => {
   const handleClick = () => {
-    !isFlipped && !isDisabled && onClick(index);
+    console.log(index, 'handleClick');
+    !isFlipped && onClick(index);
   };
+  console.log(isFlipped, 'isFlip');
   return (
     <button
-      className={`card w-16 h-10 bg-secondary ${isFlipped && 'is-flipped'}`}
+      className={`card w-16 h-10 bg-secondary ${isFlipped && 'is-flipped'} ${
+        isInactive && 'is-inactive'
+      }`}
       onClick={handleClick}>
-      <div className="card-face card-font-face">Front</div>
-      <div className="card-face card-back-face">Back</div>
+      <div className="card-face">Front</div>
+      <div className="card-back-face">{word.lang}</div>
     </button>
   );
 };
--- pages/games/memo/play.tsx
+++ pages/games/memo/play.tsx
@@ -1,34 +1,57 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import MetaHead from 'components/MetaHead';
-import Search from 'components/Search';
 import { useAppSelector } from 'redux/hooks';
 import { selectMemoState } from 'redux/slices/gamesSlice';
 import FlipCard from 'components/FlipCard';
 import { words } from 'data';
 
-type Word = {
-  id: string;
-  pol: string;
-  eng: string;
+interface Words {
+  correct: boolean;
+  [key: string]: string;
 }
+
+type OpenCards = {
+  index: number;
+};
+
+const shuffleCards = (array: Words[]) => {
+  console.log(array, 'array shuffle')
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  return array;
+};
 
 const MemoPlay = () => {
   const memoState = useAppSelector(selectMemoState);
-  const [cards, setCards] = useState<Word[]>([]);
-  const [openCards, setOpenCards] = useState([]);
+  const [cardsOne, setCardsOne] = useState<Words[]>([]);
+  const [cardsTwo, setCardsTwo] = useState<Words[]>([]);
+  const [cards, setCards] = useState(() => shuffleCards(cardsOne.concat(cardsTwo)));
+  const [openCards, setOpenCards] = useState<OpenCards[]>([]);
   const [clearedCards, setClearedCards] = useState({});
   const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
-  const timeout = useRef(null);
+  const timeout = null;
+  const [points, setPoints] = useState(0);
 
   useEffect(() => {
     words
       .filter((word) => word.cat === 'Numbers')
       .map((word) => {
-        setCards((prev) => [...prev, { id: word.id, pol: word.pol, eng: word.eng }]);
+        setCardsOne((prev) => [...prev, { id: word.id, lang: word.pol, correct: false }]);
+        setCardsTwo((prev) => [...prev, { id: word.id, lang: word.eng, correct: false }]);
       });
-      console.log(cards, 'cards');
   }, []);
+
+  useEffect(() => {
+    setCards(() => shuffleCards(cardsOne.concat(cardsTwo)));
+    console.log('pick');
+  }, [cardsOne, cardsTwo]);
 
   const disable = () => {
     setShouldDisableAllCards(true);
@@ -37,26 +60,33 @@ const MemoPlay = () => {
     setShouldDisableAllCards(false);
   };
 
+  const evaluate = () => {
+    const [first, second] = openCards;
+    enable();
+    console.log('evaluate', openCards);
+    console.log(cards[first], 'card first');
+    console.log(cards[second], 'card second');
+    if (cards[first].id === cards[second].id) {
+      console.log(cards[first], 'card first');
+      console.log(cards[second], 'card second');
+      setClearedCards((prev) => ({ ...prev, [cards[first].id]: true }));
+      setOpenCards([]);
+      setPoints((prev) => prev + 1);
+      return;
+    }
+    // This is to flip the cards back after 500ms duration
+    setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+  const handleCardClick = (index: number) => {
 
-  // const evaluate = () => {
-  //   const [first, second] = openCards;
-  //   enable();
-  //   if (cards[first].type === cards[second].type) {
-  //     setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
-  //     setOpenCards([]);
-  //     return;
-  //   }
-  //   // This is to flip the cards back after 500ms duration
-  //   timeout.current = setTimeout(() => {
-  //     setOpenCards([]);
-  //   }, 500);
-  // };
-  const handleCardClick = (index) => {
     if (openCards.length === 1) {
       setOpenCards((prev) => [...prev, index]);
+      console.log(openCards, 'open Cards');
       disable();
     } else {
-      clearTimeout(timeout.current);
+      //clearTimeout(timeout.current);
       setOpenCards([index]);
     }
   };
@@ -71,25 +101,18 @@ const MemoPlay = () => {
     };
   }, [openCards]);
 
-  const checkIsFlipped = (index) => {
+  const checkIsFlipped = (index: number) => {
     return openCards.includes(index);
   };
 
   const checkIsInactive = (card) => {
-    return Boolean(clearedCards[card.type]);
-  };
-
-  const handleRestart = () => {
-    setClearedCards({});
-    setOpenCards([]);
-    setShouldDisableAllCards(false);
-    // set a shuffled deck of cards
+    return Boolean(clearedCards[card.id]);
   };
 
   return (
-    <div className="w-screen h-auto min-h-screen flex flex-col relative font-baloo dark:bg-gray-700 dark:text-gray-100">
+    <div className="w-screen min-h-screen flex flex-col relative font-baloo dark:bg-gray-700 dark:text-gray-100">
       <MetaHead />
-      <main className="w-full max-h-screen">
+      <main className="w-full min-h-screen">
         <Link href="/games/memo" passHref>
           <button className="flex flex-row p-2 m-2 bg-primaryLight rounded-md dark:bg-primaryDark">
             <svg
@@ -110,15 +133,16 @@ const MemoPlay = () => {
           {cards.map((card, index) => (
             <FlipCard
               key={index}
-              card={card}
+              word={card}
               index={index}
-              isDisabled={shouldDisableAllCards}
+              correct={card.correct}
               isInactive={checkIsInactive(card)}
               isFlipped={checkIsFlipped(index)}
               onClick={handleCardClick}
             />
           ))}
         </section>
+        Points: {points}
       </main>
     </div>
   );
