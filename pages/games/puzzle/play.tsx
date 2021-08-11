cb4ac189a852ce8b56344b21431966a5f2c13f5b
Arek Cichocki <arexspeed95@gmail.com>
2021-08-11 17:18:11 +0200

    finish puzzle

--- pages/games/puzzle/play.tsx
+++ pages/games/puzzle/play.tsx
@@ -82,11 +82,15 @@ const PuzzlePlay = () => {
   const checkAnswer = () => {
     setCheckCorrect(true);
     let correctlyArr = [];
-    for (let i = 0; i < wordArray.length; i++) {
+    for (let i = 0; i < answer.length; i++) {
       correctlyArr.push(answer[i] === wordCorrect[i]);
     }
-    const correctly = correctlyArr.every((val, i, arr) => val === arr[0]);
-    setCorrect(correctly);
+    if (correctlyArr.length === wordCorrect.length) {
+      const correctly = correctlyArr.every((val, i, arr) => val === arr[0]);
+      setCorrect(correctly);
+    } else {
+      setCorrect(false);
+    }
   };
 
   //Style of answer correct check
@@ -140,7 +144,12 @@ const PuzzlePlay = () => {
             ) : (
               <div className="flex flex-row justify-center items-center">
                 <Flag flag={userLang.short} />
-                <span className="text-lg">{word?.lang1}</span>
+                <span className="text-lg">{word?.lang1}</span>{' '}
+                <button
+                  className="bg-primaryLight dark:bg-primaryDark p-2 mx-2 rounded-sm"
+                  onClick={() => setOpenHintWord(false)}>
+                  Hide
+                </button>
               </div>
             )}
           </article>
@@ -174,6 +183,28 @@ const PuzzlePlay = () => {
               Show answer
             </button>
           </article>
+          {correct && (
+            <article className="flex flex-col justify-center items-center w-full">
+              <p>Great you set all letters correct!!</p>
+              <p>Now you can back and try next words with more letters</p>
+              <Link href="/games/puzzle" passHref>
+                <button className="flex flex-row p-2 m-2 bg-primaryLight rounded-md dark:bg-primaryDark">
+                  <svg
+                    className="w-6 h-6"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      fillRule="evenodd"
+                      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>Puzzle</span>
+                </button>
+              </Link>
+            </article>
+          )}
         </section>
       </main>
     </div>
